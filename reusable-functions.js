51e29a6794056428f8816d581daf9e48a5a3cf1f
Rodney St Cloud <hoyrod1@aol.com>
2024-04-20 21:26:40 -0400

    Started Part 2: Thinking Methodically continued. Completed the task of removing the entries with an age greater than 50

--- reusable-functions.js
+++ reusable-functions.js
@@ -14,39 +14,39 @@ console.log(
  * Which of these examples is best? Why?
  * Note that there is no "correct" answer.
  */
-function reverseString(str) {
-  const strArray = str.split("");
-  const revArray = strArray.reverse();
-  const revString = revArray.join("");
-  return revString;
-}
+// function reverseString(str) {
+//   const strArray = str.split("");
+//   const revArray = strArray.reverse();
+//   const revString = revArray.join("");
+//   return revString;
+// }
 
-function reverseString2(str) {
-  return str.split("").reverse().join("");
-}
+// function reverseString2(str) {
+//   return str.split("").reverse().join("");
+// }
 
-function reverseString3(str) {
-  let revString = "";
-  for (let i = str.length - 1; i >= 0; i--) {
-    revString += str[i];
-  }
-  return revString;
-}
+// function reverseString3(str) {
+//   let revString = "";
+//   for (let i = str.length - 1; i >= 0; i--) {
+//     revString += str[i];
+//   }
+//   return revString;
+// }
 
-function reverseString4(str) {
-  if (str === "") return "";
-  else return reverseString4(str.substr(1)) + str.charAt(0);
-}
+// function reverseString4(str) {
+//   if (str === "") return "";
+//   else return reverseString4(str.substr(1)) + str.charAt(0);
+// }
 
-function reverseString5(str) {
-  return str === "" ? "" : reverseString5(str.substr(1)) + str.charAt(0);
-}
+// function reverseString5(str) {
+//   return str === "" ? "" : reverseString5(str.substr(1)) + str.charAt(0);
+// }
 
-console.log(reverseString("!dlroW olleH"));
-console.log(reverseString2("!dlroW olleH"));
-console.log(reverseString3("!dlroW olleH"));
-console.log(reverseString4("!dlroW olleH"));
-console.log(reverseString5("!dlroW olleH"));
+// console.log(reverseString("!dlroW olleH"));
+// console.log(reverseString2("!dlroW olleH"));
+// console.log(reverseString3("!dlroW olleH"));
+// console.log(reverseString4("!dlroW olleH"));
+// console.log(reverseString5("!dlroW olleH"));
 console.log(
   "=============================================================================="
 );
@@ -170,4 +170,10 @@ function sortArrayByAge(arrayOfObjects) {
 console.log(sortArrayByAge(arrayOfObjects));
 // console.log(arrayOfObjects[0].age);
 //==============================================================================//
+//==============================================================================//
+// This Function will remove entries with an age greater than 50 ---------------//
+function filterArrayByAge(arrayOfObjects) {
+  return arrayOfObjects.filter((objAge) => objAge.age < 50);
+}
+console.log(filterArrayByAge(arrayOfObjects));
 //==============================================================================//
